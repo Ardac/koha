7fe0bc23464e831cc294d3f60ac436e7b350bb88
Colin Campbell <colin.campbell@ptfs-europe.com>
2013-09-11 11:04:48 +0100

    (fao) addbiblio customizations for agrovoc
    Changes to the template suggest further mods may be required

--- koha-tmpl/intranet-tmpl/prog/en/js/cataloging.js
+++ koha-tmpl/intranet-tmpl/prog/en/js/cataloging.js
@@ -228,6 +228,8 @@ function CloneField(index, hideMarc, advancedMARCEditor) {
                             anchors[j].setAttribute('onclick',"UnCloneField('" + new_id + "'); return false;");
                         } else if (anchors[j].getAttribute('class') == 'expandfield') {
                             anchors[j].setAttribute('onclick',"ExpandField('" + new_id + "'); return false;");
+                        } else if (anchors[j].getAttribute('class') == 'buttonPlus') {
+                            anchors[j].setAttribute('onclick',"openAgrovoc('" + new_id + "')");
                         }
                     }
                 }
@@ -480,4 +482,8 @@ function CheckMandatorySubfields(p){
         }
     });
     return total;
+}
+
+function openAgrovoc(tagsubfield) {
+newin=window.open("../agrovoc/agrovoc.pl?lang_english='EN'&index="+tagsubfield,'width=700,height=550,toolbar=false,scrollbars=yes');
 }
